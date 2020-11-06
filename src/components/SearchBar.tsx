d23eaee8189a1e678a1e33cbccd897439ee6f97a
obrigeax <andrew.obrigewitsch@lexisnexis.com>
2020-11-06 10:14:40 -0800

    added search button

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLazyQuery } from "@apollo/client";
 import "react-bootstrap-typeahead/css/Typeahead.css";
 import { AsyncTypeahead } from "react-bootstrap-typeahead";
 import { SEARCH_FOR_ISSUES } from "../graphql/queries";
+import { Button, Col, Row } from "reactstrap";
 
 const SearchBar = ({ setSearchValue }: { setSearchValue: Function }) => {
+  const [value, setValue] = useState("");
   const [getTitles, { loading, error, data }] = useLazyQuery(SEARCH_FOR_ISSUES);
 
   error && console.log(error);
@@ -16,28 +18,45 @@ const SearchBar = ({ setSearchValue }: { setSearchValue: Function }) => {
     : [];
 
   return (
-    <AsyncTypeahead
-      onSearch={(selected) => {
-        !loading &&
-          getTitles({
-            variables: { query: `${selected} repo:facebook/react in:title` },
-          });
-      }}
-      id={"Search"}
-      searchText={"Searching ..."}
-      delay={500}
-      options={typeAheadData.filter((i: any) => i)}
-      isLoading={loading}
-      placeholder="Please type complete words..."
-      minLength={2}
-      clearButton
-      onChange={(selected) => setSearchValue(selected[0])}
-      onKeyDown={(e: any) => {
-        if (e.key === "Enter") {
-          setSearchValue(e.target.value);
-        }
-      }}
-    />
+    <Row>
+      <Col>
+        <AsyncTypeahead
+          onSearch={(selected) => {
+            !loading &&
+              getTitles({
+                variables: {
+                  query: `${selected} repo:facebook/react in:title`,
+                },
+              });
+          }}
+          id={"Search"}
+          searchText={"Searching ..."}
+          delay={500}
+          options={typeAheadData.filter((i: any) => i)}
+          isLoading={loading}
+          placeholder="Please type complete words..."
+          minLength={2}
+          clearButton
+          onChange={(selected) => setSearchValue(selected[0])}
+          onKeyDown={(e: any) => {
+            if (e.key === "Enter") {
+              setSearchValue(e.target.value);
+            }
+          }}
+          onBlur={(e: any) => setValue(e.target.value)}
+        />
+      </Col>
+      <Col sm={"2"}>
+        <Button
+          color="primary"
+          onClick={() => {
+            setSearchValue(value);
+          }}
+        >
+          Search
+        </Button>
+      </Col>
+    </Row>
   );
 };
 
